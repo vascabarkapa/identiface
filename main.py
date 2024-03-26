44b218b96154509616e581aac9fdcb2734eb5c31
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2024-03-26 15:22:18 +0100

    face recognition with age and gender prediction

--- main.py
+++ main.py
@@ -28,13 +28,44 @@ def faceBox(faceNet, frame):
 faceProto = "face_detector/opencv_face_detector.pbtxt"
 faceModel = "face_detector/opencv_face_detector_uint8.pb"
 
+ageProto = "age/age_deploy.prototxt"
+ageModel = "age/age_net.caffemodel"
+
+genderProto = "gender/gender_deploy.prototxt"
+genderModel = "gender/gender_net.caffemodel"
+
 faceNet = cv2.dnn.readNet(faceModel, faceProto)
+ageNet = cv2.dnn.readNet(ageModel, ageProto)
+genderNet = cv2.dnn.readNet(genderModel, genderProto)
+
+MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
+ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
+genderList = ['Male', 'Female']
 
 video = cv2.VideoCapture(0)
 
+padding = 20
+
 while True:
     ret,frame=video.read()
-    detect = faceBox(faceNet, frame)
+    frame,bboxs = faceBox(faceNet, frame)
+
+    for bbox in bboxs:
+        face = frame[max(0,bbox[1]-padding):min(bbox[3]+padding,frame.shape[0]-1),max(0,bbox[0]-padding):min(bbox[2]+padding, frame.shape[1]-1)]
+        blob = cv2.dnn.blobFromImage(frame, 1.0, (227,227), MODEL_MEAN_VALUES, swapRB=False)
+
+        genderNet.setInput(blob)
+        genderPrediction = genderNet.forward()
+        gender = genderList[genderPrediction[0].argmax()]
+
+        ageNet.setInput(blob)
+        agePrediction = ageNet.forward()
+        age = ageList[agePrediction[0].argmax()]
+
+        label = "{},{}".format(gender, age)
+        cv2.rectangle(frame, (bbox[0], bbox[1]-30), (bbox[2], bbox[1]), (0,255,0), -1)
+        cv2.putText(frame, label, (bbox[0], bbox[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255,255,255), 2, cv2.LINE_AA)
+
     cv2.imshow("Identiface", frame)
     k = cv2.waitKey(1)
 
